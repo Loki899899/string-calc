fa3cbf8977a165a03059223e008dfcb2187d1d3e
Loki <lokeshsingh899@gmail.com>
2025-09-29 20:40:54 +0530

    step 4, allow delimiters in string

--- calc.js
+++ calc.js
@@ -1,7 +1,15 @@
-const stringCalc = (inputString) => {
+const stringCalc = (inputString = "") => {
     if (!inputString) return 0;
 
-    const nums = inputString.split(/\n|,/);
+    let delimiter = /\n|,/;
+    const hasDelim = inputString.search(/\/\/.+\n/)
+    if (hasDelim > -1) {
+        const index = inputString.indexOf("\n");
+        delimiter = inputString.slice(2, index);
+        inputString = inputString.slice(index);
+    }
+
+    const nums = inputString.split(RegExp(delimiter));
     result = 0;
     
     nums.forEach(num => {
@@ -15,3 +23,4 @@ console.log(stringCalc(""))
 console.log(stringCalc("1"))
 console.log(stringCalc("1,5"))
 console.log(stringCalc("1\n2,3"))
+console.log(stringCalc("//;\n1;2"))
