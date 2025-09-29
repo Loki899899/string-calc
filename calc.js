116b5ef2e13b0756926f93a3ae9d54b466a704ca
Loki <lokeshsingh899@gmail.com>
2025-09-29 21:19:01 +0530

    step 8, add support for multiple delims
    step 7 already works
    add extract delim fn to get multiple delims if present
    change delim defaults and values to array

--- calc.js
+++ calc.js
@@ -1,17 +1,35 @@
 const ALLOWED_MAX = 1000
 
+const delimiterRegex = /\[(.*?)\]/g;
+
+const extractDelims = (input) => {
+  const delims = [];
+  let m;
+  
+  while ((m = delimiterRegex.exec(input)) !== null) {
+    delims.push(m[1]);
+  }
+  
+  return delims;
+}
+
 const stringCalc = (inputString = "") => {
     if (!inputString) return 0;
 
-    let delimiter = /\n|,/;
+    let delimiter = ["\n|,"];
     const hasDelim = inputString.search(/\/\/.+\n/)
     if (hasDelim > -1) {
         const index = inputString.indexOf("\n");
-        delimiter = inputString.slice(2, index);
+        const multipleDelim = extractDelims(inputString.slice(2, index));
+        if (multipleDelim.length) {
+            delimiter = multipleDelim.map(d => d.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'));;
+        } else {
+            delimiter = [inputString.slice(2, index)];
+        }
         inputString = inputString.slice(index);
     }
 
-    const nums = inputString.split(RegExp(delimiter));
+    const nums = inputString.split(RegExp(delimiter.join("|")));
     result = 0;
     let negatives = [];
     for(const num of nums) {
@@ -55,3 +73,7 @@ try {
 }
 
 console.log(stringCalc("//;\n2;1001")) // 2
+
+console.log(stringCalc("//[***]\n1***2***3")) // 6
+
+console.log(stringCalc("//[*][%]\n1*2%3")) // 6
